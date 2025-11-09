49984560fd1bd04923cca92d0c21cba4cf3b66c4
Ayush Barthwal <ayushbarthwal0101@gmail.com>
2025-11-09 23:29:55 +0530

    Heroku fix

--- Backend/api_server.py
+++ Backend/api_server.py
@@ -130,6 +130,10 @@ def schedule():
 
     return jsonify(results)
 
+@app.route('/')
+def index():
+    return "Backend is running!"
+
 @app.route('/api/integration_outputs/<filename>')
 def get_integration_output(filename):
     return send_from_directory(INTEGRATION_DIR, filename)
@@ -147,4 +151,5 @@ def get_report(filename):
     return send_from_directory(REPORTS_DIR, filename)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5050))
+    app.run(host='0.0.0.0', port=port)
